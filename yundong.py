90f877159f5a48b7eed35fa8b7dd5513ecafd1ac
luziyang11 <58388239+luziyang11@users.noreply.github.com>
2025-02-16 12:18:47 +0800

    Create yundong.py

---
+++ yundong.py
@@ -0,0 +1,34 @@
+import requests
+from random import randint
+
+# 定义请求的URL
+url = "http://yanwan.store/run4/mi20241027.php"
+
+# 定义请求头
+headers = {
+    "Host": "yanwan.store",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0",
+    "Accept": "*/*",
+    "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+    "Accept-Encoding": "gzip, deflate",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Content-Length": "52",
+    "Origin": "http://bs.yanwan.store",
+    "Connection": "keep-alive",
+    "Referer": "http://bs.yanwan.store/"
+}
+
+# 定义请求体
+step = str(14000+randint(0, 4000))
+data = {
+    "user": "laoeluziyang@163.com",
+    "password": "asd123456!",
+    "step": step
+}
+
+# 发送POST请求
+response = requests.post(url, headers=headers, data=data)
+
+# 输出响应内容
+print(response.status_code)
+print(response.json())
