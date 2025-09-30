10dfd690c36ef62a0c246abc670881fda97f67c9
luziyang11 <58388239+luziyang11@users.noreply.github.com>
2025-09-30 15:57:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import requests
 from random import randint
 import push ,os
+import datetime
+
+def generate_token():
+    """
+    生成基于当前时间的 token，计算方式：
+    token = (年份 * (月份 + 日期)) + (日期 * 小时 * 分钟)
+    """
+    now = datetime.datetime.now()
+    year = now.year
+    month = now.month  # Python 的月份是 1-12，不需要 +1
+    day = now.day
+    hour = now.hour
+    minute = now.minute
+
+    token = (year * (month + day)) + (day * hour * minute)
+    return str(token)
 
 # 定义请求的URL
 url = "https://bs.yanwan.store/run4/mi20241029.php"
@@ -11,12 +27,12 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0",
     "Accept": "*/*",
     "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-    "Accept-Encoding": "gzip, deflate",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     "Content-Length": "52",
     "Origin": "https://bs.yanwan.store",
     "Connection": "keep-alive",
-    "Referer": "https://bs.yanwan.store/"
+    "Referer": "https://bs.yanwan.store/run4/"
 }
 ##MY_API_KEY=os.environ.get("MY_API_KEY")
 #user=os.environ.get("MY_ACCOUNT")
@@ -29,8 +45,10 @@ step = str(16000+randint(0, 3000))
 data = {
     "user": user,
     "password": password,
-    "step": step
+    "step": step,
+    "token":generate_token()
 }
+
 
 # 发送POST请求
 response = requests.post(url, headers=headers, data=data)
