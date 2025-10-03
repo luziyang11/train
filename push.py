7fca7e491c2a86dbf13eb120a637874d075d36cc
luziyang11 <58388239+luziyang11@users.noreply.github.com>
2025-10-03 16:38:39 +0800

    Update push.py

--- push.py
+++ push.py
@@ -2,11 +2,13 @@ import requests,os
 MY_API_KEY=os.environ.get("MY_API_KEY")
 #MY_API_KEY="SCT171556TUitezllQAZBQWUhyDWRIY9RK"
 def send_to_wechat(message):
+    print(message)
     url = f"https://sctapi.ftqq.com/{MY_API_KEY}.send"
     data = {
         "title": "Github_微信运动步数：",  # 消息标题
         "desp": message          # 消息内容（支持 Markdown）
     }
+    print(data)
     response = requests.post(url, data=data)
     if response.status_code == 200:
         print("消息推送成功！")
